1ca437f64d7f5e5d42fe6d1eeb293a4827ac357c
gary <gary@MACSTATION.local>
2018-12-31 16:28:17 +0000

    Initial commit

---
+++ api/src/plumbing/claimsMiddleware.ts
@@ -0,0 +1,89 @@
+import {NextFunction, Request, Response} from 'express';
+import {AuthorizationMicroservice} from '../logic/authorizationMicroservice';
+import {ApiLogger} from '../plumbing/apiLogger';
+import {ResponseWriter} from '../plumbing/responseWriter';
+import {Authenticator} from './authenticator';
+import {ClaimsCache} from './claimsCache';
+
+/*
+ * An entry point class for claims processing
+ */
+export class ClaimsMiddleware {
+
+    /*
+     * Fields
+     */
+    private _authenticator: Authenticator;
+    private _authorizationMicroservice: AuthorizationMicroservice;
+
+    /*
+     * Receive configuration
+     */
+    public constructor(authenticator: Authenticator, authorizationMicroservice: AuthorizationMicroservice) {
+        this._authenticator = authenticator;
+        this._authorizationMicroservice = authorizationMicroservice;
+    }
+
+    /*
+     * Authorize a request and return true if authorization checks pass
+     */
+    public async authorizeRequestAndSetClaims(
+        request: Request,
+        response: Response,
+        next: NextFunction): Promise<boolean> {
+
+        // First read the token from the request header and report missing tokens
+        const accessToken = this._readToken(request.header('authorization'));
+        if (accessToken === null) {
+            ApiLogger.info('Claims Middleware', 'No access token received');
+            ResponseWriter.writeInvalidTokenResponse(response);
+            return false;
+        }
+
+        // Bypass validation and use cached results if they exist
+        const cachedClaims = ClaimsCache.getClaimsForToken(accessToken);
+        if (cachedClaims !== null) {
+            response.locals.claims = cachedClaims;
+            return true;
+        }
+
+        // Otherwise start by introspecting the token
+        const result = await this._authenticator.validateTokenAndGetTokenClaims(accessToken);
+
+        // Handle invalid or expired tokens
+        if (!result.isValid) {
+            ApiLogger.info('Claims Middleware', 'Invalid or expired access token received');
+            ResponseWriter.writeInvalidTokenResponse(response);
+            return false;
+        }
+
+        // Next add central user info to claims
+        await this._authenticator.getCentralUserInfoClaims(result.claims!, accessToken);
+
+        // Next add product user data to claims
+        await this._authorizationMicroservice.getProductClaims(result.claims!, accessToken);
+
+        // Next cache the results
+        ClaimsCache.addClaimsForToken(accessToken, result.expiry!, result.claims!);
+
+        // Then move onto the API controller to execute business logic
+        ApiLogger.info('Claims Middleware', 'Claims lookup completed successfully');
+        response.locals.claims = result.claims;
+        return true;
+    }
+
+    /*
+     * Try to read the token from the authorization header
+     */
+    private _readToken(authorizationHeader: string | undefined): string | null {
+
+        if (authorizationHeader) {
+            const parts = authorizationHeader.split(' ');
+            if (parts.length === 2 && parts[0] === 'Bearer') {
+                return parts[1];
+            }
+        }
+
+        return null;
+    }
+}
---
+++ api/src/plumbing/errorHandler.ts
@@ -0,0 +1,144 @@
+import {ApiError} from './apiError';
+import {ApiLogger} from './apiLogger';
+import {ClientError} from './clientError';
+
+/*
+ * A class to handle composing and reporting errors
+ */
+export class ErrorHandler {
+
+    /*
+     * Handle the server error and get client details
+     */
+    public static handleError(exception: any): ClientError {
+
+        // Ensure that the exception has a known type
+        const handledError = ErrorHandler.fromException(exception);
+        if (exception instanceof ClientError) {
+
+            // Client errors mean the caller did something wrong
+            const clientError = handledError as ClientError;
+
+            // Log the error
+            const errorToLog = clientError.toLogFormat();
+            ApiLogger.error(JSON.stringify(errorToLog));
+
+            // Return the API response to the caller
+            return clientError;
+
+        } else {
+
+            // API errors mean we experienced a failure
+            const apiError = handledError as ApiError;
+
+            // Log the error with an id
+            const errorToLog = apiError.toLogFormat();
+            ApiLogger.error(JSON.stringify(errorToLog));
+
+            // Return the API response to the caller
+            return apiError.toClientError();
+        }
+    }
+
+    /*
+     * Ensure that all errors are of a known type
+     */
+    public static fromException(exception: any): ApiError | ClientError {
+
+        // Already handled 500 errors
+        if (exception instanceof ApiError) {
+            return exception;
+        }
+
+        // Already handled 4xx errors
+        if (exception instanceof ClientError) {
+            return exception;
+        }
+
+        // Well coded errors should derive from this base class
+        if (exception instanceof Error) {
+
+            const apiError = new ApiError({
+                message: `Problem encountered`,
+                area: 'Exception',
+                details: exception.message,
+            });
+            apiError.stack = exception.stack;
+            return apiError;
+        }
+
+        // For other errors we just call toString
+        return new ApiError({
+            message: 'Problem encountered',
+            area: 'Exception',
+            details: exception.toString(),
+        });
+    }
+
+    /*
+     * Handle the request promise error for metadata lookup failures
+     */
+    public static fromMetadataError(responseError: any, url: string): ApiError {
+
+        const apiError = new ApiError({
+            statusCode: 500,
+            area: 'Metadata Lookup',
+            url,
+            message: 'Metadata lookup failed',
+        });
+        ErrorHandler._updateErrorFromHttpResponse(apiError, responseError);
+        return apiError;
+    }
+
+    /*
+     * Handle the request promise error for introspection failures
+     */
+    public static fromIntrospectionError(responseError: any, url: string): ApiError {
+
+        const apiError = new ApiError({
+            statusCode: 500,
+            area: 'Token Validation',
+            url,
+            message: 'Token validation failed',
+        });
+        ErrorHandler._updateErrorFromHttpResponse(apiError, responseError);
+        return apiError;
+    }
+
+    /*
+     * Handle user info lookup failures
+     */
+    public static fromUserInfoError(responseError: any, url: string): ApiError {
+
+        // Already handled expired errors
+        if (responseError instanceof ApiError) {
+            return responseError;
+        }
+
+        const apiError = new ApiError({
+            statusCode: 500,
+            area: 'User Info',
+            url,
+            message: 'User info lookup failed',
+        });
+        ErrorHandler._updateErrorFromHttpResponse(apiError, responseError);
+        return apiError;
+    }
+
+    /*
+     * Update error fields with response details
+     */
+    private static _updateErrorFromHttpResponse(apiError: ApiError, responseError: any): void {
+
+        if (responseError.error && responseError.error_description) {
+
+            // Include OAuth error details if returned
+            apiError.message += ` : ${responseError.error}`;
+            apiError.details = responseError.error_description;
+        } else {
+
+            // Otherwise capture exception details
+            apiError.details = responseError.toString();
+        }
+    }
+}
