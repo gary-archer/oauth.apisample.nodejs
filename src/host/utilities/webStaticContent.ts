d2e57f5d368ef24cb4a6b9ae59bd55389850aefe
gary <gary@MACSTATION.local>
2020-02-23 20:00:25 +0000

    Updates

--- src/host/utilities/webStaticContent.ts
+++ src/host/utilities/webStaticContent.ts
@@ -5,7 +5,8 @@ import path from 'path';
  * Relative paths to web content, from other code samples in parallel folders to this one
  */
 const WEB_FILES_ROOT = '../../../../authguidance.websample.final/spa';
-const DESKTOP_FILES_ROOT = '../../../../authguidance.desktopsample1/web';
+const LOOPBACK_FILES_ROOT = '../../../../authguidance.desktopsample1/web';
+const DESKTOP_FILES_ROOT = '../../../../authguidance.desktopsample.final/web';
 const MOBILE_FILES_ROOT = '../../../../authguidance.mobilesample.android';
 
 /*
@@ -36,17 +37,24 @@ export class WebStaticContent {
     }
 
     /*
-     * When our desktop loopback sample is run against a local API, serve desktop web content
+     * The post login page for our initial desktop app, which uses loopback logins
      */
-    public handleDesktopRequests(): void {
+    public handleDesktopLoopbackRequests(): void {
 
-        // These are custom post login pages to render in the orphaned browser session
-        this._expressApp.get('/desktop/loginsuccess.html', this._desktopLoginSuccessPage);
-        this._expressApp.get('/desktop/loginerror.html', this._desktopLoginErrorPage);
+        this._expressApp.get('/desktop/postlogin.html', this._loopbackPostLoginPage);
     }
 
     /*
-     * When our Android is run against a local API, serve mobile web content to avoid Chrome Custom Tab hangs
+     * When our desktop private URI scheme sample is run against a local API, serve post login web content
+     */
+    public handleDesktopRequests(): void {
+
+        this._expressApp.get('/desktop/postlogin.html', this._desktopPostLoginPage);
+        this._expressApp.get('/desktop/postLogout.html', this._desktopPostLogoutPage);
+    }
+
+    /*
+     * When our Android app is run against a local API, serve mobile web content
      */
     public handleAndroidRequests(): void {
 
@@ -94,18 +102,26 @@ export class WebStaticContent {
     }
 
     /*
-     * Serve up the desktop login success page
+     * Serve up the loopback post login page
      */
-    private _desktopLoginSuccessPage(request: express.Request, response: express.Response): void {
-        const webFilePath = path.join(`${__dirname}/${DESKTOP_FILES_ROOT}/loginsuccess.html`);
+    private _loopbackPostLoginPage(request: express.Request, response: express.Response): void {
+        const webFilePath = path.join(`${__dirname}/${LOOPBACK_FILES_ROOT}/postlogin.html`);
         response.sendFile(webFilePath);
     }
 
     /*
-     * Serve up the desktop login error page
+     * Serve up the desktop post login page
      */
-    private _desktopLoginErrorPage(request: express.Request, response: express.Response): void {
-        const webFilePath = path.join(`${__dirname}/${DESKTOP_FILES_ROOT}/loginerror.html`);
+    private _desktopPostLoginPage(request: express.Request, response: express.Response): void {
+        const webFilePath = path.join(`${__dirname}/${DESKTOP_FILES_ROOT}/postlogin.html`);
+        response.sendFile(webFilePath);
+    }
+
+    /*
+     * Serve up the desktop post logout page
+     */
+    private _desktopPostLogoutPage(request: express.Request, response: express.Response): void {
+        const webFilePath = path.join(`${__dirname}/${DESKTOP_FILES_ROOT}/postlogout.html`);
         response.sendFile(webFilePath);
     }
 
