fa7c8ed8424fb54dc0b263c25a188c36ee2ba94b
gary <gary@MACSTATION.local>
2020-10-21 18:57:33 +0100

    Updates

--- deployment/deployApi.sh
+++ deployment/deployApi.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
 #
-# A MacOS script to build the Node API and deploy it to a local PC minikube Kubernetes cluster
+# A MacOS script to deploy 2 instances of the Node API to a local PC Minikube Kubernetes cluster
 #
-
-#
-# Use the Minikube Docker Daemon rather than that of Docker Desktop for Mac
-#
-echo "Preparing Kubernetes ..."
-eval $(minikube docker-env)
 
 #
 # Clean up any resources for the previously deployed version of the API
@@ -16,10 +10,9 @@ eval $(minikube docker-env)
 kubectl delete deploy/nodeapi               2>/dev/null
 kubectl delete svc/nodeapi-svc              2>/dev/null
 docker image rm -f nodeapi                  2>/dev/null
-kubectl delete secret mycompany-com-tls     2>/dev/null
 
 #
-# Build the docker image, with the Node files and other resources
+# Build the docker image, to package NodeJS files and other resources
 #
 echo "Building NodeJS Docker Image ..."
 cd ..
@@ -29,11 +22,6 @@ then
   echo "*** Docker build error ***"
   exit 1
 fi
-
-#
-# Deploy our SSL wildcard certificate to the Kubernetes cluster
-#
-kubectl create secret tls mycompany-com-tls --cert=../certs/mycompany.ssl.crt --key=../certs/mycompany.ssl.key
 
 #
 # Deploy 2 instances of the local docker image to 2 Kubernetes pods
@@ -51,6 +39,5 @@ fi
 # Expose the API to clients outside Kubernetes on port 443 with a custom host name
 # We can then access the API at https://nodeapi.mycompany.com/api/companies
 #
-minikube addons enable ingress
 kubectl apply -f ingress.yaml
 echo "Deployment completed successfully"
