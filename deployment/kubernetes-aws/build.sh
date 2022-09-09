9d48442413ca696a496f03fa813021c0f72a8817
gary <gary@MACSTATION.local>
2022-09-09 18:29:33 +0100

    Updates

--- deployment/kubernetes-aws/build.sh
+++ deployment/kubernetes-aws/build.sh
@@ -49,19 +49,19 @@ fi
 #
 # Copy in the internal cluster root CA from the parent project, to be trusted within the container
 #
-copy ../../../certs/mycluster.ca.pem deployment/kubernetes-local/trusted.ca.pem
+copy ../../../certs/mycluster.ca.pem deployment/shared/trusted.ca.pem
 
 #
 # On Windows, fix problems with trailing newline characters in Docker scripts
 #
 if [ "$PLATFORM" == 'WINDOWS' ]; then
-  sed -i 's/\r$//' deployment/docker/docker-init.sh
+  sed -i 's/\r$//' deployment/shared/docker-init.sh
 fi
 
 #
 # Build the Docker container
 #
-docker build --no-cache -f deployment/docker/Dockerfile --build-arg TRUSTED_CA_CERTS='deployment/kubernetes-local/trusted.ca.pem' -t "$DOCKERHUB_ACCOUNT/finalnodejsapi:v1" .
+docker build --no-cache -f deployment/shared/Dockerfile --build-arg TRUSTED_CA_CERTS='deployment/shared/trusted.ca.pem' -t "$DOCKERHUB_ACCOUNT/finalnodejsapi:v1" .
 if [ $? -ne 0 ]; then
   echo '*** API docker build problem encountered'
   exit 1
