52c1d42e4428acb32353cc361105f0cab80dfd21
gary <gary@MACSTATION.local>
2022-09-10 07:21:41 +0100

    Updates

--- deployment/shared/docker-init.sh
+++ deployment/shared/docker-init.sh
@@ -8,23 +8,21 @@
 # See if the extra trusted certificates file is non-empty
 #
 TRUSTED_CA_CERTS='/usr/local/share/certificates/trusted.ca.crt'
-if [ -s "$TRUSTED_CA_CERTS" ]; then
 
-  #
-  # If so then add the certificates tool
-  #
-  apk --no-cache add ca-certificates
-  if [ $? -ne 0 ]; then
-    echo 'Problem encountered adding the ca-certificates package'
-    exit 1
-  fi
-    
-  #
-  # Configure operating system trust
-  #
-  update-ca-certificates
-  if [ $? -ne 0 ]; then
-    echo 'Problem encountered updating root certificates'
-    exit 1
-  fi
+#
+# Add the certificates tool
+#
+apk --no-cache add ca-certificates
+if [ $? -ne 0 ]; then
+  echo 'Problem encountered adding the ca-certificates package'
+  exit 1
+fi
+  
+#
+# Configure operating system trust
+#
+update-ca-certificates
+if [ $? -ne 0 ]; then
+  echo 'Problem encountered updating root certificates'
+  exit 1
 fi
