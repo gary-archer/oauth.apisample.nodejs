ab69f764085ec1b1adaeb3b8c3ae07b8f370038a
gary <garyarcher36@gmail.com>
2022-11-20 11:36:06 +0000

    Updates

--- test/scripts/run_wiremock.sh
+++ test/scripts/run_wiremock.sh
@@ -10,11 +10,16 @@ cd ../..
 
 #
 # Run Wiremock over HTTPS in this terminal
-# On Linux ensure that you have first granted Node.js permissions to listen on port 447:
-# - sudo setcap 'cap_net_bind_service=+ep' $(which node)
+# On Linux ensure that you have first granted Java the permissions to listen on port 447:
+# - sudo setcap 'cap_net_bind_service=+ep' /usr/lib/jvm/zulu-17-amd64/bin/java
 #
-./node_modules/.bin/wiremock --root-dir test/integration --https-port 447 --disable-http \
---https-keystore './certs/authsamples-dev.ssl.p12' --keystore-type 'pkcs12' --keystore-password 'Password1' --key-manager-password 'Password1'
+./node_modules/.bin/wiremock \
+--root-dir test/integration \
+--https-port 447 --disable-http \
+--https-keystore './certs/authsamples-dev.ssl.p12' \
+--keystore-type 'pkcs12' \
+--keystore-password 'Password1' \
+--key-manager-password 'Password1'
 
 #
 # Prevent automatic terminal closure
