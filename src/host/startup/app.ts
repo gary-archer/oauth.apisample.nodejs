712d315c69cfd36cc5afd0bf63b87840941e8f15
gary-archer <garyarcher36@gmail.com>
2024-05-02 13:17:01 +0100

    Updates

--- src/host/startup/app.ts
+++ src/host/startup/app.ts
@@ -5,30 +5,24 @@ import {LoggerFactoryBuilder} from '../../plumbing/logging/loggerFactoryBuilder.
 import {Configuration} from '../configuration/configuration.js';
 import {HttpServerConfiguration} from './httpServerConfiguration.js';
 
-/*
- * The application entry point
- */
-(async () => {
+// Create initial objects
+const loggerFactory = LoggerFactoryBuilder.create();
+const container = new Container();
 
-    // Create initial objects
-    const loggerFactory = LoggerFactoryBuilder.create();
-    const container = new Container();
+try {
 
-    try {
+    // Load our JSON configuration and configure logging
+    const configurationBuffer = await fs.readFile('api.config.json');
+    const configuration = JSON.parse(configurationBuffer.toString()) as Configuration;
+    loggerFactory.configure(configuration.logging);
 
-        // Load our JSON configuration and configure logging
-        const configurationBuffer = await fs.readFile('api.config.json');
-        const configuration = JSON.parse(configurationBuffer.toString()) as Configuration;
-        loggerFactory.configure(configuration.logging);
+    // Configure the API behaviour at startup
+    const httpServer = new HttpServerConfiguration(configuration, container, loggerFactory);
+    await httpServer.configure();
+    httpServer.start();
 
-        // Configure the API behaviour at startup
-        const httpServer = new HttpServerConfiguration(configuration, container, loggerFactory);
-        await httpServer.configure();
-        httpServer.start();
+} catch (e) {
 
-    } catch (e) {
-
-        // Report startup errors
-        loggerFactory.logStartupError(e);
-    }
-})();
+    // Report startup errors
+    loggerFactory.logStartupError(e);
+}
