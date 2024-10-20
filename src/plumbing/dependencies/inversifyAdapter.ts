e153f165c151f60d18784341a1e277c47f3d0770
gary-archer <garyarcher36@gmail.com>
2024-10-20 19:05:20 +0100

    Updates

--- src/plumbing/dependencies/inversifyAdapter.ts
+++ src/plumbing/dependencies/inversifyAdapter.ts
@@ -1,6 +1,9 @@
 import {Action, ClassConstructor, IocAdapter} from 'routing-controllers';
 import {Container} from 'inversify';
 
+/*
+ * Tell the routing controllers library to resolve objects with the child container for the current request
+ */
 export class InversifyAdapter implements IocAdapter {
 
     private readonly container: Container;
