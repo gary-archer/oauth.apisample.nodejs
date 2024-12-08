8386fc09cec2204ca110f5ef656220f20bb148f3
gary-archer <garyarcher36@gmail.com>
2024-12-08 21:34:52 +0000

    Updates

--- deployment/docker/build.sh
+++ deployment/docker/build.sh
@@ -40,7 +40,7 @@ fi
 #
 # Build the docker image
 #
-docker build -f deployment/shared/Dockerfile -t finalnodejsapi:latest .
+docker build -t finalnodejsapi:latest .
 if [ $? -ne 0 ]; then
   echo 'Problem encountered building the API docker image'
   exit
