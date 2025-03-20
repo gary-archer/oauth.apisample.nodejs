f5a73fab135fe1f33b1d2a0ab4efc29e097a66a4
gary-archer <garyarcher36@gmail.com>
2025-03-20 19:02:27 +0000

    Updates

--- src/plumbing/middleware/authorizerMiddleware.ts
+++ src/plumbing/middleware/authorizerMiddleware.ts
@@ -4,7 +4,6 @@ import {ClaimsPrincipal} from '../claims/claimsPrincipal.js';
 import {ClaimsReader} from '../claims/claimsReader.js';
 import {BASETYPES} from '../dependencies/baseTypes.js';
 import {LogEntryImpl} from '../logging/logEntryImpl.js';
-import {UnhandledExceptionHandler} from '../middleware/unhandledExceptionHandler.js';
 import {OAuthFilter} from '../oauth/oauthFilter.js';
 
 /*
@@ -17,31 +16,19 @@ export class AuthorizerMiddleware {
      */
     public async execute(request: Request, response: Response, next: NextFunction): Promise<void> {
 
-        // Get the container for this request
+        // Get objects
         const container = response.locals.container as Container;
+        const filter =  container.get<OAuthFilter>(BASETYPES.OAuthFilter);
+        const logEntry = container.get<LogEntryImpl>(BASETYPES.LogEntry);
 
-        try {
+        // Run the authorizer then log identity details
+        const claimsPrincipal = await filter.execute(request, response);
+        logEntry.setIdentity(ClaimsReader.getStringClaim(claimsPrincipal.getJwt(), 'sub'));
 
-            // Get objects
-            const filter =  container.get<OAuthFilter>(BASETYPES.OAuthFilter);
-            const logEntry = container.get<LogEntryImpl>(BASETYPES.LogEntry);
+        // Bind claims to this requests's child container so that they are injectable into business logic
+        container.bind<ClaimsPrincipal>(BASETYPES.ClaimsPrincipal).toConstantValue(claimsPrincipal);
 
-            // Run the authorizer then log identity details
-            const claimsPrincipal = await filter.execute(request, response);
-            logEntry.setIdentity(ClaimsReader.getStringClaim(claimsPrincipal.getJwt(), 'sub'));
-
-            // Bind claims to this requests's child container so that they are injectable into business logic
-            container.bind<ClaimsPrincipal>(BASETYPES.ClaimsPrincipal).toConstantValue(claimsPrincipal);
-
-            // On success, move on to the controller logic
-            next();
-
-        } catch (e) {
-
-            // Handle authorization exceptions
-            const exceptionHandler = container.get<UnhandledExceptionHandler>(
-                BASETYPES.UnhandledExceptionHandler);
-            exceptionHandler.execute(e, request, response, next);
-        }
+        // On success, move on to the controller logic
+        next();
     }
 }
